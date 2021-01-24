83110fee2ee014cfa2ed8bae5824646599859bd8
amitKSingh26 <amit06132@gmail.com>
2021-01-24 14:36:09 +0530

    id name classname locators eg added

--- crediArtId/src/test/java/locators/IdTagNameNameClassName.java
+++ crediArtId/src/test/java/locators/IdTagNameNameClassName.java
@@ -1,20 +1,15 @@
 package locators;
-
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-
 public class IdTagNameNameClassName
 {
 	//all locators are static method present inside By class.
 	public static void main(String[] args) throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver","E://mydiskB//jar//chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","E://mydiskB//jar//chromedriverLatest.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.navigate().to("https://www.calculator.net/mass-calculator.html");
@@ -24,9 +19,16 @@ public class IdTagNameNameClassName
 		WebElement density = driver.findElement(By.id("cdensity"));
 		density.clear();
 		density.sendKeys("1000");
+		
+		//----------examples for Name------------
+		WebElement volume = driver.findElement(By.name("cvolume"));
+		volume.clear();
+		volume.sendKeys("5");
 		//----------examples for tagname------------
 
 		//----------examples for classname------------
-		
+		Thread.sleep(5000);
+		WebElement clrBtn = driver.findElement(By.className("clearbtn"));
+		clrBtn.clear();
 	}
 }
